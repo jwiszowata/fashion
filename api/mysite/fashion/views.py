625f0e3935105e0a6c5b8f3d2e07e4d4de4e5673
Joanna Wiszowata <wiszowata.joanna@gmail.com>
2020-01-14 01:23:31 +0100

    Visual updates

--- api/mysite/fashion/views.py
+++ api/mysite/fashion/views.py
@@ -19,13 +19,16 @@ import numpy as np
 def index(request):
 	# Load test data
 	test_images = np.array([random.randint(0, 1) for i in range(28*28*2)])
-	test_labels = [3, 5]
 	test_images = test_images / 255.
-	print(test_images)
 	test_images.shape = (2, 28, 28, 1)
+
+	class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 
+                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
 	# Recreate the exact same model purely from the file
 	model = keras.models.load_model('../../training/model.h5')
 
+	example_result = model.predict(test_images)
 	# Re-evaluate the model
-	loss, acc = model.evaluate(test_images, test_labels, verbose=2)
-	return HttpResponse("Restored model, accuracy: {:5.2f}%, {:5.2f}".format(100 * acc, loss))
+	# loss, acc = model.evaluate(test_images, test_labels, verbose=2)
+	return HttpResponse("Restored model, On random data classes prediction: " + class_names[np.argmax(example_result[0])] + ", " + class_names[np.argmax(example_result[1])])
